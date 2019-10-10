0d28f8680ed579419a03a2997f0885a9518806d5
Hui Lin <huilin1618@gmail.com>
2019-10-10 17:49:17 -0400

    Updates

---
+++ precache-manifest.ef7fc0833539cfdbfa4b3a4f595c5d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d6d08de981ce371520f5841140a4eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be86f69e99951cd80b6",
+    "url": "/static/css/main.8d9d01c4.chunk.css"
+  },
+  {
+    "revision": "3d8b6eb3e89667e1203f",
+    "url": "/static/js/2.6245dbbb.chunk.js"
+  },
+  {
+    "revision": "9be86f69e99951cd80b6",
+    "url": "/static/js/main.272b5873.chunk.js"
+  },
+  {
+    "revision": "2c0e06afefeb68de689c",
+    "url": "/static/js/runtime-main.2cbd3329.js"
+  }
+]);
