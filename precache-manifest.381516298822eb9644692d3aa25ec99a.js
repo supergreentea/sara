80faa2e0d5970ab1ccf09c4c88d0f563053a4d9a
Hui Lin <huilin1618@gmail.com>
2019-10-10 17:19:14 -0400

    Updates

---
+++ precache-manifest.381516298822eb9644692d3aa25ec99a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60be8d9f9f19bd092f94e6e303ad372",
+    "url": "/sara/index.html"
+  },
+  {
+    "revision": "fbbcfad4dd2be2384258",
+    "url": "/sara/static/css/main.8d9d01c4.chunk.css"
+  },
+  {
+    "revision": "d0b5fbb13cb26606afbf",
+    "url": "/sara/static/js/2.451ec702.chunk.js"
+  },
+  {
+    "revision": "fbbcfad4dd2be2384258",
+    "url": "/sara/static/js/main.39fe2781.chunk.js"
+  },
+  {
+    "revision": "a447e68c141227fcc8b5",
+    "url": "/sara/static/js/runtime-main.29158c58.js"
+  }
+]);
